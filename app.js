96ed17b4b4b2443b4558402d496ed03b25decb77
amit negi <amitnegi6341@gmail.com>
2026-08-17 20:49:57 +0530

    routers

--- app.js
+++ app.js
@@ -1,29 +1,11 @@
 const express=require('express');
+const orderRouter=require('./route/orders')
+const userRouter=require('./route/users')
 const app=express()
 app.use(express.json())
 
-let users=[
-    {id:1,name:"alice"},
-    {id:2,name:"bob"}
-];
-
-app.get('/users',(req,res)=>{
-    res.json(users)
-})
-app.get('/users/:id',(req,res)=>{
-    const userId=parseInt(req.params.id);
-    const user=users.find(u=>u.id==userId)
-    if(!user){
-        return res.status(404).json({message:'user not found'})
-    }
-    res.json(user)
-})
-app.post('/users',(req,res)=>{
-    const {name}=req.body;
-    const newUser={id:users.length+1,name};
-    users.push(newUser);
-    res.status(201).json(newUser)
-})
+app.use('/orders',orderRouter)
+app.use('/users',userRouter)
 
 const port=3000
 app.listen(port,()=>{

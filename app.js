e1d057d6a428a433fa20088cfc020cc3ad65c14d
amit negi <amitnegi6341@gmail.com>
2026-08-18 17:15:26 +0530

    crud crud

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express=require('express');
-const orderRouter=require('./route/orders')
-const userRouter=require('./route/users')
+const studentRoute=require('./route/student')
+const courseRoute=require('./route/courses')
 const app=express()
 app.use(express.json())
 
-app.use('/orders',orderRouter)
-app.use('/users',userRouter)
+app.get('/', (req, res) => {
+    res.send('Welcome to the Student & Course Portal API!');
+});
+app.use('/students',studentRoute)
+app.use('/courses',courseRoute)
 
+app.use((req, res) => {
+    res.status(404).send('Page not found');
+});
 const port=3000
 app.listen(port,()=>{
     console.log(`Server is running`);
